83e42245cbb1e337eb5050d7a4028cd6ea38df41
RishabhGupta <rishabh852768@gmail.com>
2022-04-07 13:36:09 +0530

    cs removed

--- src/App.js
+++ src/App.js
@@ -14,8 +14,7 @@ const spotifyApi = new SpotifyWebApi({
 })
 spotifyApi.setAccessToken('BQCuU8YqmCOqdC9p9DRz6voyZ-vAdicjBrrbNGj89hhwstNug9yf7vMl1WKN4BEMu_LGFYG0dzBAgGgov97zxW9RGgWPEpN2oNVtr8lgBA8Zo_Lw5p4kcwmJ7-YUc4CtBeSt11wW0eq8sku-v9RAQuOPe2L4MlJci-8');
 function App() {
-  const CLIENT_ID = "f7d19cc8039b471d87f32ba9c5a7fdd6";
-  const CLIENT_SECRET ="BQB9r60DXk66xyvLldr3Jh_PCK0qUUlfVrfnpPbxZtb";
+
   const token = localStorage.setItem("token", "BQC2pQVNv8P3u_8NSitnr58pEtPPXKBcK-NPuf7r7n3XrNrmgDnZ8qmZbLozUivpq86wgsBEUhkG56Hd9xX3ngCSWg2cJNaSBqj4mjqWOOPRyVsDrh-NUakSxN7d2X75raCxBtk8GA-3afYwpvrg801PfN1nRLaqObo");
 
   useEffect(()=>{
